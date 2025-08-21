0e1fefaa54c6306fccae93d249cf101b78f1d866
Associazione Beni Comuni Stefano Rodotà <abcbenicomuni@gmail.com>
2025-08-21 12:48:09 +0200

    Update form-template.php

--- templates/form-template.php
+++ templates/form-template.php
@@ -1,10 +1,14 @@
 <form method="post">
-  <label>Numero totale di posti di lavoro (q1101)</label>
-  <input type="number" name="q1101" required>
+  <?php wp_nonce_field('bs_form_action', 'bs_form_nonce'); ?>
 
-  <label>Entrate totali annue (q1203)</label>
-  <input type="number" name="q1203" required>
+  <label for="q1101"><?php _e('Numero totale di posti di lavoro', 'bilanciosociale'); ?></label>
+  <input type="number" name="q1101" id="q1101" required>
 
-  <input type="submit" name="bs_submit" value="Invia">
+  <label for="q1203"><?php _e('Entrate totali annue', 'bilanciosociale'); ?></label>
+  <input type="number" name="q1203" id="q1203" required>
+
+  <input type="submit" name="bs_submit" value="<?php _e('Invia', 'bilanciosociale'); ?>">
 </form>
+
+
 
