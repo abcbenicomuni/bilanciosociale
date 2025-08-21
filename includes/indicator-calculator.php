393e473e72e9eba947595bb5b5ee5a51510117f6
Associazione Beni Comuni Stefano Rodotà <abcbenicomuni@gmail.com>
2025-08-21 12:54:56 +0200

    Update indicator-calculator.php

--- includes/indicator-calculator.php
+++ includes/indicator-calculator.php
@@ -2,21 +2,21 @@
 function bs_calculate_indicators($data) {
     $indicators = [];
 
-    if (!empty($data['q1202']) && !empty($data['q1201'])) {
-        $indicators['ind5'] = ($data['q1202'] / $data['q1201']) * 100;
+    if (!empty($data['q1203']) && !empty($data['q1101'])) {
+        $indicators['ind1'] = ($data['q1203'] / $data['q1101']);
     }
 
     foreach ($indicators as $code => $value) {
-        if ($value > 80) {
-            $indicators[$code . '_judgment'] = 'Ottimo';
-        } elseif ($value > 50) {
-            $indicators[$code . '_judgment'] = 'Buono';
-        } else {
-            $indicators[$code . '_judgment'] = 'Critico';
-        }
+        $indicators[$code . '_judgment'] = match (true) {
+            $value > 100000 => 'Ottimo',
+            $value > 50000 => 'Buono',
+            default => 'Critico',
+        };
     }
 
     update_user_meta(get_current_user_id(), 'bs_indicators', $indicators);
 }
 ?>
+
+
 
