1aacd81abca3248eb61bccee1ece7a6b1d71882f
Associazione Beni Comuni Stefano Rodotà <abcbenicomuni@gmail.com>
2025-08-21 12:20:18 +0200

    Create form-template.php

---
+++ templates/form-template.php
@@ -0,0 +1,10 @@
+<form method="post">
+  <label>Numero totale di posti di lavoro (q1101)</label>
+  <input type="number" name="q1101" required>
+
+  <label>Entrate totali annue (q1203)</label>
+  <input type="number" name="q1203" required>
+
+  <input type="submit" name="bs_submit" value="Invia">
+</form>
+
